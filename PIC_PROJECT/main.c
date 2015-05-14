e8c1a79c97b14a02ad12b8d2658e8a83cbf973e3
Daniel Staves <danstaves@gmail.com>
2015-05-14 13:55:14 -0600

    Initial step into adding PWM Codes for actuating Servos

--- PIC_PROJECT/main.c
+++ PIC_PROJECT/main.c
@@ -1,10 +1,3 @@
-/* 
- * File:   main.c
- * Author: Scott
- *
- * Created on May 5, 2015, 12:07 PM
- */
-
 #define USE_AND_OR /* To enable AND_OR mask setting */
 #include <p24FV32KA301.h>
 #include "MPU6050.h"
@@ -22,11 +15,14 @@
 _FOSCSEL(FNOSC_FRC);          // 8MHz oscillator
 
 void Setup_Timer1();
+void Setup_PWM();
 void Setup_I2C1(void);
-/*
- * MAIN CODE BEGINS
- */
-int main(int argc, char** argv)
+void Actuate_Servo(unsigned short servoNum, float angle);
+
+unsigned int OC4R;
+volatile unsigned int* const PWMReg[] = {&OC1R, &OC2R, &OC3R, &OC4R};
+
+int main()
 {
     int error = 1;
     ANSELA=0x0000;
@@ -46,16 +42,82 @@ int main(int argc, char** argv)
 
     Calibrate_Gyros();
     Zero_Sensors();
-    Setup_Timer1();
-    IEC0bits.T1IE = 1;
-    while(1)
-    {
 
-    }
+    Setup_Timer1();
+    Setup_PWM();
+
+    Actuate_Servo(0,0);
+    while(1){}
     
     return (EXIT_SUCCESS);
 }
+void Actuate_Servo(unsigned short servoNum, float angle)
+{
+    /* Servo moves between 0 degrees and 180 degrees, 90 degrees is along rocket body */
+    unsigned int OCR = 315+(angle/180)*943;
+    *PWMReg[servoNum] = OCR;
+}
+void Setup_PWM()
+{
+    /* OC1: Pin11
+     * OC2: Pin4
+     * OC3: Pin5
+     * All PWM pins use Timer 2
+     * Simulated PWM uses Timer 3  */
 
+    //Pin Setup
+    _TRISA1 = 0;
+    _TRISB0 = 0;
+    _TRISB1 = 0;
+    _TRISB7 = 0;
+    _ANSA1 = 0;
+    _ANSB0 = 0;
+    _ANSB1 = 0;
+
+    //Timer 2 Initialization
+    T2CONbits.T32 = 0b0; //Use Timer2 as a 16 bit timer
+    T2CONbits.TSIDL = 0b0; //Continue in Idle mode
+    T2CONbits.TCS = 0b0; //Use internal clock
+    T2CONbits.TGATE = 0b0; //Disable Gated Time Accumulation
+    T2CONbits.TCKPS = 0b01; //Use 1:8 Prescaling mode
+    PR2 = 10486;    //Period Value for a 50Hz timer with 1:8 prescaling mode and 8mhz clock
+
+    //Timer 3 Initialization
+    T3CONbits.TSIDL = 0b0; //Continue in Idle mode
+    T3CONbits.TCS = 0b0; //Use internal clock
+    T3CONbits.TGATE = 0b0; //Disable Gated Time Accumulation
+    T3CONbits.TCKPS = 0b01; //Use 1:8 Prescaling mode
+    _T3IE = 1;
+    _T3IF = 0;
+    _T3IP = 5;
+    PR3 = 10486;    //Period Value for a 50Hz timer with 1:8 prescaling mode and 8mhz clock
+
+    //Edge-Aligned PWM mode
+    OC1CON1bits.OCM = 0b110;
+    OC2CON1bits.OCM = 0b110;
+    OC3CON1bits.OCM = 0b110;
+
+    //Select Timer 2
+    OC1CON1bits.OCTSEL = 0b000;
+    OC2CON1bits.OCTSEL = 0b000;
+    OC3CON1bits.OCTSEL = 0b000;
+
+    //Continue when CPU is idle
+    OC1CON1bits.OCSIDL = 0b0;
+    OC2CON1bits.OCSIDL = 0b0;
+    OC3CON1bits.OCSIDL = 0b0;
+
+    //Synchronize with Timer 2
+    OC1CON2bits.SYNCSEL = 0b01100;
+    OC2CON2bits.SYNCSEL = 0b01100;
+    OC3CON2bits.SYNCSEL = 0b01100;
+
+    //Initialize Duty Cycle for 1.5 ms pulse (90 degrees)
+    OC1R = 786;
+    OC2R = 786;
+    OC3R = 786;
+    OC4R = 786;
+}
 void Setup_I2C1(void)
 {
     //******EXAMPLE INITIALIZATION******
@@ -117,7 +179,10 @@ void Setup_Timer1()
 	T1CONbits.TCKPS = 0b11; //Timer prescale 1:1, 1:8, 1:64, 1:256
 	//T1CONbits.T32 = 0; //32 bit timer disabled
 	T1CONbits.TCS = 0; //Internal clock source
-	IPC0bits.T1IP = 0b100; //Priority 4
+
+	_T1IP = 0b100; //Priority 4
+        _T1IF = 0;
+        _T1IE = 1;
 
 	//Frequency of 400Hz
 	PR1 = timerCount; //Period register
@@ -141,4 +206,17 @@ void __attribute__((interrupt,no_auto_psv)) _T1Interrupt()
     Get_Accel_Angles();
     _T1IE = 1;
 
+}
+
+void __attribute__((interrupt,no_auto_psv)) _T3Interrupt()
+{
+    _T1IF = 0;
+    if (TMR3 <= OC4R)
+    {
+        _LATA1 = 1;
+    }
+    else
+    {
+        _LATA1 = 0;
+    }
 }
